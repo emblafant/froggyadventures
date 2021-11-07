767ebc69762eb5d837f96da377705991147cdd7c
emblafant <embla98wallen@gmail.com>
2021-11-07 23:52:08 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,60 @@
+const codes =  [
+  {
+    code: "1111",
+    message: "hello"
+  },
+  {
+    code: "2222",
+    message: "no"
+  },
+  {
+    code: "Aaaa",
+    message: "bye"
+  }
+];
+
+const responses = {
+  default: "Enter your code!",
+  faulty: "Wrong code!!!"
+};
+
+const body = document.querySelector("body");
+const monsterText = document.querySelector("#speech-bubble-text");
+const illustration = document.querySelector("#monster-illustration");
+
+const button = document.querySelector("#submit-button");
+button.addEventListener("click", () => {
+  const inputCode = document.querySelector("#input").value;
+
+  let match = false;
+  let angryFrog = false; 
+
+  if (inputCode.length === 4) {
+    codes.forEach(code => {
+      if (inputCode.toUpperCase() === code.code.toUpperCase()) {
+        angryFrog = false;
+        monsterText.innerText = code.message;
+        match = true;
+        button.innerText = "Again!" 
+        button.addEventListener("click", () =>{
+            location.reload();
+          })
+        };
+      });
+  }
+
+  if (!match) {
+    document.querySelector("#input").value = "";
+    monsterText.innerText = responses.faulty;
+    angryFrog = true;
+  };
+
+  if (angryFrog) {
+    illustration.src="Angry-frog.PNG";
+    body.style.backgroundColor = "#f2aea3";
+  } else {
+    illustrtion.src="Happy-frog.PNG";
+    body.style.backgroundColor = "#9abaa0";
+  }
+
+});
