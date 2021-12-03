3361039b388508bb803faf4ec75c7982941ae701
emblafant <embla98wallen@gmail.com>
2021-12-03 16:59:59 +0100

    added codes

--- main.js
+++ main.js
@@ -1,20 +1,434 @@
 const codes =  [
   {
-    code: "1111",
-    message: "hello"
+    number: 1,
+    code: "XHBY",
+    message: "Go camping"
   },
   {
-    code: "2222",
-    message: "no"
+    number: 2,
+    code: "S1OQ",
+    message: "Take an early morning hike"
   },
   {
-    code: "Aaaa",
-    message: "bye"
+    number: 3,
+    code: "W3AY",
+    message: "Climb a tree"
   },
   {
-    code: "3333",
+    number: 4,
+    code: "A2NY",
+    message: "Put mentos in coke"
+  },
+  {
+    number: 5,
+    code: "M91R",
+    message: "Inhale helium"
+  },
+  {
+    number: 6,
+    code: "UYG1",
+    message: "Text your “Number Neighbour” and see what happens"
+  },  {
+    number: 7,
+    code: "RY2C",
+    message: "Be “Yes Man” for a day"
+  },  {
+    number: 8,
+    code: "LICO",
     message: "Tie pillows to your arms and torso, then have a sumo wrestling match"
-  }
+  },  {
+    number: 9,
+    code: "TINC",
+    message: "Have a water balloon fight"
+  },  {
+    number: 10,
+    code: "CFYB",
+    message: "Make a funny tear off flyer and put it somewhere public"
+  },  {
+    number: 11,
+    code: "T5YW",
+    message: "Learn how to tie 10 types of knots"
+  },  {
+    number: 12,
+    code: "7MIV",
+    message: "Learn the Cyrillic Script"
+  },  {
+    number: 13,
+    code: "IYUL",
+    message: "Make a vodka-spiked watermelon"
+  },  {
+    number: 14,
+    code: "TH9N",
+    message: "Bullfika!!!"
+  },  {
+    number: 15,
+    code: "XNBC",
+    message: "Make Smorgastarta"
+  },  {
+    number: 16,
+    code: "03V0",
+    message: "Have a picnic"
+  },  {
+    number: 17,
+    code: "NW4D",
+    message: "Plant a veggie or fruit seed that you have at home"
+  },  {
+    number: 18,
+    code: "FGDT",
+    message: "Invent your own board game"
+  },  {
+    number: 19,
+    code: "NMER",
+    message: "Play charades"
+  },  {
+    number: 20,
+    code: "U4YY",
+    message: "Color in a coloring book"
+  },  {
+    number: 21,
+    code: "MVGY",
+    message: "Make slime"
+  },  {
+    number: 22,
+    code: "50GM",
+    message: "Make your own ice cream"
+  },  {
+    number: 23,
+    code: "CETQ",
+    message: "Camp indoors"
+  },  {
+    number: 24,
+    code: "39IE",
+    message: "Write a letter to someone"
+  },  {
+    number: 25,
+    code: "U83O",
+    message: "Complete a crossword"
+  },  {
+    number: 26,
+    code: "LIBX",
+    message: "Write a song"
+  },  {
+    number: 27,
+    code: "AA8M",
+    message: "Learn how to juggle"
+  },  {
+    number: 28,
+    code: "O7U8",
+    message: "Learn how to yo-yo"
+  },  {
+    number: 29,
+    code: "6TW1",
+    message: "Bake bread"
+  },  {
+    number: 30,
+    code: "IKSW",
+    message: "Write a haiku"
+  },  {
+    number: 31,
+    code: "A78K",
+    message: "Go 24 hours without electronics"
+  },  {
+    number: 32,
+    code: "9NF6",
+    message: "Go to a live performance"
+  },  {
+    number: 33,
+    code: "GHHU",
+    message: "Make a mixtape"
+  },  {
+    number: 34,
+    code: "TBOG",
+    message: "Spend a day outside, in costume"
+  },  {
+    number: 35,
+    code: "7YDS",
+    message: "Compte with someone on who can make the best memes"
+  },  {
+    number: 36,
+    code: "ZVQJ",
+    message: "Make long-term bets with someone"
+  },  {
+    number: 37,
+    code: "6ZXV",
+    message: "Make up a drinking game and play it"
+  },  {
+    number: 38,
+    code: "HZ1Z",
+    message: "Buy a magazine and read it"
+  },  {
+    number: 39,
+    code: "6ME0",
+    message: "Read a Kalle Anka Pocket"
+  },  {
+    number: 40,
+    code: "RNIX",
+    message: "Book a cheap flight somewhere"
+  },  {
+    number: 41,
+    code: "4AFE",
+    message: "Blow bubbles"
+  },  {
+    number: 42,
+    code: "PFQB",
+    message: "Try a new fruit"
+  },  {
+    number: 43,
+    code: "LFOG",
+    message: "Go eat at a restaurant without underwear"
+  },  {
+    number: 44,
+    code: "FULH",
+    message: "Visit all the monopoly properties in one day"
+  },  {
+    number: 45,
+    code: "KMRQ",
+    message: "Visit Skansen"
+  },  {
+    number: 46,
+    code: "G9XO",
+    message: "Visit a museum you’ve never been to"
+  },  {
+    number: 47,
+    code: "69YZ",
+    message: "Go down to the water"
+  },  {
+    number: 48,
+    code: "9V6F",
+    message: "Play Strip Uno"
+  },  {
+    number: 49,
+    code: "3LOV",
+    message: "Repair something broken"
+  },  {
+    number: 50,
+    code: "4PPO",
+    message: "Go to a tourist spot you haven’t visited"
+  },  {
+    number: 51,
+    code: "CDDE",
+    message: "Take a friend and draw each other"
+  },  {
+    number: 52,
+    code: "HRU6",
+    message: "Go to the toy store and buy something for yourself"
+  },  {
+    number: 53,
+    code: "ZAFR",
+    message: "Go spend the night at a hotel"
+  },  {
+    number: 54,
+    code: "Q498",
+    message: "Send a postcard to someone"
+  },  {
+    number: 55,
+    code: "TAWK",
+    message: "Have a game night with a grand prize"
+  },  {
+    number: 56,
+    code: "QSLC",
+    message: "Go on a city bus tour"
+  },  {
+    number: 57,
+    code: "45PV",
+    message: "Have a sleepover at home"
+  },  {
+    number: 58,
+    code: "JFL8",
+    message: "Go see a play"
+  },  {
+    number: 59,
+    code: "QERS",
+    message: "Solve a Rubik’s cube"
+  },  {
+    number: 60,
+    code: "EXJ4",
+    message: "Call a friend you haven’t talked to in a while"
+  },  {
+    number: 61,
+    code: "2QTP",
+    message: "Take a train somewhere"
+  },  {
+    number: 62,
+    code: "1N9C",
+    message: "Spend the day hunting Pokémon"
+  },  {
+    number: 63,
+    code: "KR9H",
+    message: "Make milkshakes"
+  },  {
+    number: 64,
+    code: "UJV8",
+    message: "Try a new restaurant"
+  },  {
+    number: 65,
+    code: "ZK5J",
+    message: "Try Lebanese food"
+  },  {
+    number: 66,
+    code: "QJ9L",
+    message: "Make trolldeg"
+  },  {
+    number: 67,
+    code: "VSSC",
+    message: "Fold origami"
+  },  {
+    number: 68,
+    code: "GJ2A",
+    message: "Go to a café in Gamla Stan"
+  },  {
+    number: 69,
+    code: "INUC",
+    message: "Visit Moderna Museet"
+  },  {
+    number: 70,
+    code: "GOA9",
+    message: "Go to ICEBAR"
+  },  {
+    number: 71,
+    code: "QN26",
+    message: "Play a new card game"
+  },  {
+    number: 72,
+    code: "3JB1",
+    message: "Code a mini game"
+  },  {
+    number: 73,
+    code: "9UNI",
+    message: "Build a fort"
+  },  {
+    number: 74,
+    code: "KS07",
+    message: "Play Club Penguin"
+  },  {
+    number: 75,
+    code: "PG9Q",
+    message: "Create a Roblox account"
+  },  {
+    number: 76,
+    code: "V63C",
+    message: "Get a massage"
+  },  {
+    number: 77,
+    code: "OPW7",
+    message: "Explore a suburb you’ve never been to before"
+  },  {
+    number: 78,
+    code: "Q8R5",
+    message: "Prank someone"
+  },  {
+    number: 79,
+    code: "7TCP",
+    message: "Go swim"
+  },  {
+    number: 80,
+    code: "OZ68",
+    message: "Eat cake with your hands"
+  },  {
+    number: 81,
+    code: "MM54",
+    message: "Write an essay about something that interests you"
+  },  {
+    number: 82,
+    code: "QD20",
+    message: "Make up a new cocktail"
+  },  {
+    number: 83,
+    code: "WUJS",
+    message: "Eat street food"
+  },  {
+    number: 84,
+    code: "62VT",
+    message: "Visit Fjarilshuset"
+  },  {
+    number: 85,
+    code: "PY0X",
+    message: "Finish a short online course"
+  },  {
+    number: 86,
+    code: "HUBB",
+    message: "Wake up early and watch the sunrise"
+  },  {
+    number: 87,
+    code: "GC0O",
+    message: "Visit a national park"
+  },  {
+    number: 88,
+    code: "7QI4",
+    message: "Make sushi"
+  },  {
+    number: 89,
+    code: "U4GB",
+    message: "Learn how to whistle"
+  },  {
+    number: 90,
+    code: "ZB0F",
+    message: "Get dressed up for a night out"
+  },  {
+    number: 91,
+    code: "XUQ9",
+    message: "Visit a farm"
+  },  {
+    number: 92,
+    code: "ZRLE",
+    message: "Design your own T-Shirt"
+  },  {
+    number: 93,
+    code: "FI0R",
+    message: ""
+  },  {
+    number: 94,
+    code: "",
+    message: ""
+  },  {
+    number: 95,
+    code: "",
+    message: ""
+  },  {
+    number: 96,
+    code: "",
+    message: ""
+  },  {
+    number: 97,
+    code: "",
+    message: ""
+  },  {
+    number: 98,
+    code: "",
+    message: ""
+  },  {
+    number: 99,
+    code: "",
+    message: ""
+  },  {
+    number: 100,
+    code: "",
+    message: ""
+  },  {
+    number: 101,
+    code: "",
+    message: ""
+  },  {
+    number: 102,
+    code: "",
+    message: ""
+  },  {
+    number: 103,
+    code: "",
+    message: ""
+  },  {
+    number: 104,
+    code: "",
+    message: ""
+  },  {
+    number: 105,
+    code: "",
+    message: ""
+  },  {
+    number: 106,
+    code: "",
+    message: ""
+  },
 ];
 
 const responses = {
